a7c8357586edd3521ff73febbbd7931d3e719afe
chente-chester <vivalarazad@gmail.com>
2021-07-23 10:13:21 -0500

    Aplicacion Base Lista

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,8 +4,12 @@ import { useParams } from "react-router-dom";
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
 // Componentes
+import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 // Imagen
@@ -14,10 +18,32 @@ import NoImage from "../images/no_image.jpg";
 const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
+
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.budget}
+      />
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
